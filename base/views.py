6446bb65b8cac8478135a6105f0a4a88072e3282
MRBOX <olamigokeadebayo90@gmail.com>
2024-05-11 16:06:15 +0100

    Added and modified the user edit profile

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # Create your views here.
 
@@ -199,5 +199,13 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
     
-    return render(request, 'base/update-user.html')
+    if request.method == "POST":
+        form =  UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    
+    return render(request, 'base/update-user.html', {'form':form})
